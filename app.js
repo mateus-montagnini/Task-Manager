a5da0f6edbb84964cea649e04e70804b9d950199
Mateus-Montagnini <mateus.r.montagnini@gmail.com>
2023-03-19 17:06:41 -0300

    npm install dotenv

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks');
 const connectDB = require('./db/connect');
-require('dotenv').config();
 const notFound = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler')
 
